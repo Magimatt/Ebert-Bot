3eff00d2a7f3a32735eafe21a6eed38d9f68878f
Magimatt <paewrfalx@gmail.com>
2021-12-26 16:01:20 -0700

    Rework subcommands so that both 'list all' and 'list search' can be used.

--- commands/list.js
+++ commands/list.js
@@ -1,7 +1,10 @@
 /*
  MAJOR ISSUE NEEDS TO BE RESOLVED!!!!!!
- EMBED REPLIES CAN ONLY BE 10 FIELDS LONG!
- NEED TO SPLIT EMBED REPLIES INTO MULTIPLE IF INPUT ARRAY IS LONGER THAN 10.
+ EMBED REPLIES CAN ONLY BE 25 FIELDS LONG!
+
+ NEED TO SPLIT EMBED REPLIES INTO MULTIPLE IF INPUT ARRAY
+ CONTAINS MORE THAT 1024 CHARACTERS IN THE FIELD[VALUE]
+ (OR 6000 MAX CHARACTERS IN THE EMBED).
 */
 
 // Requires
@@ -17,67 +20,83 @@ module.exports = {
         .setName('list')
         .setDescription('The watched/unwatched movie lists.')
 
-        // Sub command to search through the watched/unwatched movie list
+        // Subcommand to just list all entries in the movie list
+        .addSubcommand(subcommand => 
+            subcommand
+                .setName('all')
+                .setDescription('Display all entries in the watched/unwatched lists'))
+
+        // Subcommand to search through the watched/unwatched movie list
         .addSubcommand(subcommand =>
             subcommand
                 .setName("search")
                 .setDescription("Returns a list of movie titles matching the search query")
-                .addStringOption(option => option.setName("query".setDescription("The search query")))),
+                .addStringOption(option => option.setName("query").setDescription("The search query"))),
     
     async execute(interaction) {
-        if (interaction.options.getSubCommand() === "search") {
+        // reserve the variables that will be set in the conditional
+        var watchedArr;
+        var unwatchedArr;
+        var embedColor;
+        var embedTitle;
+        var embedDescription;
+
+        // subcommand execution conditional
+        if (interaction.options.getSubcommand() === "search") {
             // Seach the name fields in the watched/unwatched arrays and builds the new arrays with the results
             const queryStr = interaction.options.getString("query");
-            const watchedArr = searchJSON(watchedjson, queryStr);
-            const unwatchedArr = searchJSON(unwatchedjson, queryStr);
+            watchedArr = searchJSON(watchedjson, queryStr);
+            unwatchedArr = searchJSON(unwatchedjson, queryStr);
 
             // If subcommand 'search' is used then build embed with following params
-            const embedColor = '#487987';
-            const embedTitle = 'Search Movie Lists';
-            const embedDescription = `Results for query: "${interaction.options.getString("query")}"`;
-        } else {
+            embedColor = '#487987';
+            embedTitle = 'Search Movie Lists';
+            embedDescription = `Results for query: "${interaction.options.getString("query")}"`;
+        } else if (interaction.options.getSubcommand() === "all") {
             // build the watched and unwatched arrays
-            const watchedArr = buildMovieArray(watchedjson);
-            const unwatchedArr = buildMovieArray(unwatchedjson);            
+            watchedArr = buildMovieArray(watchedjson);
+            unwatchedArr = buildMovieArray(unwatchedjson);            
             
             // if 'list' command is used then build the embed with the following params
-            const embedColor = '#391f43';
-            const embedTitle = 'Podcast Movie List';
-            const embedDescription = 'This is the list of all watched/unwatched movies';
+            embedColor = '#391f43';
+            embedTitle = 'Podcast Movie List';
+            embedDescription = 'This is the list of all watched/unwatched movies';
         }
-        
+
         // take the movie arrays and make hyperlink strings for the field values
         const watchedlinkstring = buildEmbedFieldValue(watchedArr);
         const unwatchedlinkstring = buildEmbedFieldValue(unwatchedArr);
         
-        // Build the embed
+        // Build the embed and fill the watched and unwatched embed fields
         const replyembed = buildMessageEmbed(embedColor, embedTitle, embedDescription);
-        
-        // fill the watched and unwatched embed fields
         replyembed.addField('Watched Movies', watchedlinkstring);
         replyembed.addField('Unwatched Movies', unwatchedlinkstring);
         
+        // send embed reply
         await interaction.reply({ embeds: [replyembed] });
     },
 };
 
 // searchJSON helper func. returns boolean instead of .search()'s integers
 function containsString(str, condition) {
-    const result = str.search(condition);
+    const result = str.toLowerCase().search(condition.toLowerCase());
     return result != -1 ? true : false;
 }
 
 // search the json for string match
 function searchJSON(jsonObj, queryString) {
     const resultArr = [];
-    for (obj of jsonObj) {
+    jsonObj.forEach(obj => {
         if (containsString(obj.name, queryString) || containsString(obj.year, queryString)) {
-            resultArr.push( { name:jsonObj.name, year:jsonObj.year, URL:jsonObj.URL } );
+            resultArr.push([obj.name, obj.year, obj.URL ]);
         }
-    }
+    });
     // if the result array is empty then push an object that will show up as no reults in the embed
-    return resultArr.length == 0 ? resultArr.push( { name:'Search returned 0 results', year:'', URL:'' } ) : resultArr
-}
+    if (resultArr.length == 0) {
+        resultArr.push(['Search returned 0 results', '', '']);
+    }
+    return resultArr
+};
 
 // build the message embed
 function buildMessageEmbed(color, title, description) {
